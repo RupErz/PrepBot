52cbfc43e30da289ae6a3b26b55e384becd95837
Nghia <nghiavu144@gmail.com>
2025-07-28 14:28:06 -0500

    Setting up route group

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RootLayout = ({children} : 
+    {children: React.ReactNode}
+) => {
+    return (
+        <div>layout</div>
+    )
+}
+
+export default RootLayout
