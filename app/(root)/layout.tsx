05bf65d55ea1e60d4b845f6c14dfb5acc38d9bc2
Nghia <nghiavu144@gmail.com>
2025-07-28 21:45:25 -0500

    Finish layout section for home route

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const RootLayout = ({children} : 
     {children: React.ReactNode}
 ) => {
     return (
-        <div>layout</div>
+        <div className='root-layout'>
+            <nav>
+                <Link href="/" className='flex items-center gap-2'>
+                    <Image 
+                        src="/logo.svg"
+                        alt='logo'
+                        width={38}
+                        height={32}
+                    />
+                    <h2 className='text-primary-100'>PrepBot</h2>
+                </Link>
+            </nav>
+            {children}
+        </div>
     )
 }
 
